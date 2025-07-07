c67439df96390dfab7ec520072f5dae9b9c47219
Guilherme Hasselmann <g.hasselmann@ventureslab.io>
2025-07-07 10:59:09 -0300

    feat(get-sale-models): Create get sales models

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+public class GetSaleProfile : Profile
+{
+    public GetSaleProfile()
+    {
+        CreateMap<GetSaleResult, GetSaleResponse>();
+        CreateMap<GetSaleItemResult, GetSaleItemResponse>();
+        CreateMap<GetSaleRequest, GetSaleCommand>();
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleRequest.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+public class GetSaleRequest
+{
+    public Guid Id { get; set; }
+}
