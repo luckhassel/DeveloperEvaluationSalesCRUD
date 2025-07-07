268b50caf8f39bd4b948d65a79fba82b0b84e627
Guilherme Hasselmann <g.hasselmann@ventureslab.io>
2025-07-07 11:14:10 -0300

    feat(delete-sale-models): Add delete sales models

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleResult.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public class DeleteSaleResult
+{
+    public Guid Id { get; set; }
+    public bool Success { get; set; }
+}
